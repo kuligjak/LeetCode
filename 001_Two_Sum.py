873c3cd0a9b5a1be7a05761ef96506b3818bcc9f
kuligjak <107987399+kuligjak@users.noreply.github.com>
2023-09-12 19:57:02 +0200

    Add files via upload

---
+++ 001_Two_Sum.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/two-sum/
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        elements = {}
+        for i, num in enumerate(nums):
+            target_gap = target - num
+            if target_gap in elements:
+                return sorted([i, elements[target_gap]])
+            else:
+                elements[num] = i
